aef12f6ca7052d45faf79d2ec378ccdb8bffced0
Sindre Bøyum <sindre.boyum@gmail.com>
2021-07-28 22:02:02 +0200

    fix possible null

--- web/netlify/functions/validation.ts
+++ web/netlify/functions/validation.ts
@@ -1,11 +1,23 @@
 import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
-  const guess = event.queryStringParameters.guess;
+  const guess = event.queryStringParameters?.guess;
+
+  if (!guess) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Missing guess",
+        headers: event.headers,
+        isCorrect: false,
+      }),
+    };
+  }
 
   const body = JSON.stringify({
     isCorrect: guess.toLowerCase() === "coldplay - speed of sound",
-    headers: event.headers
+    headers: event.headers,
+    message: null,
   });
 
   return {
