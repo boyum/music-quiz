254a8aafeafcee4f70d6492c2b92b4f27172ada8
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-15 20:19:40 +0100

    add `no-default-export` rule

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+// eslint-disable-next-line import/no-default-export
+export default MusicQuiz;
--- web/pages/api/hello.ts
+++ web/pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = (request: NextApiRequest, response: NextApiResponse<Data>): void => {
+  response.status(200).json({ name: "John Doe" });
+};
+
+// eslint-disable-next-line import/no-default-export
+export default handler;
