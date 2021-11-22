c00a26fe83fb1c91556551405d6d282562993471
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-22 08:48:15 +0100

    remove question union type from calendar day
    Because Netlify build doesn't like it

--- web/src/types/CalendarDay.ts
+++ web/src/types/CalendarDay.ts
@@ -1,5 +1,10 @@
-import { Question } from "./Question";
-
-export type CalendarDay = Question & {
+export type CalendarDay = {
+  id: string;
+  publishedAt: string;
+  slug: string | null;
+  title: string;
+  audioTrackUrl: string;
+  hints: Array<string> | null;
+  previewTitle: string;
   dayIndex: number;
 };
