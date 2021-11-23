e53fa0e92b79abd1edfea3d8da93519dd1b68f61
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-23 11:04:49 +0100

    make `CalendarDay` inherit properties from `Question`

--- web/src/types/CalendarDay.ts
+++ web/src/types/CalendarDay.ts
@@ -1,10 +1,5 @@
-export type CalendarDay = {
-  id: string;
-  publishedAt: string;
-  slug: string | null;
-  title: string;
-  audioTrackUrl: string;
-  hints: Array<string> | null;
-  previewTitle: string;
+import { Question } from "./Question";
+
+export type CalendarDay = Question & {
   dayIndex: number;
 };
