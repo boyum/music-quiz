bca376d9ee065ff29cecf9605bee9605f32a5335
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-23 15:21:00 +0100

    remove `dotenv`

--- web/src/utils/sanity-client.ts
+++ web/src/utils/sanity-client.ts
@@ -1,10 +1,6 @@
 import client from "@sanity/client";
-import dotenv from "dotenv";
 import { sanity } from "../../client-config";
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
-});
 
 /**
  * Set manually. Find configuration in
