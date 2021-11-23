09bad6214ccbd4bbd3e0d20fa37556422507cf88
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-23 10:32:08 +0100

    don't purge production styles
    This removes all class styles. This should be fixed before turning it on
    again

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,8 @@
 
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
+  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
