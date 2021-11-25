4d58dd3537ffa3b35a1626cbb5ad795691d86334
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-25 11:57:22 +0100

    set sanity api version to v1

--- web/client-config.ts
+++ web/client-config.ts
@@ -1,6 +1,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 export const sanity = {
+  apiVersion: "1",
   projectId: process.env.SANITY_PROJECT_ID || "0q6ju337",
   dataset: process.env.SANITY_DATASET || (isProduction ? "production" : "test"),
 };
