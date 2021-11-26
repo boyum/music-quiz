83f82a380183e09c5a8e94ea0a1964da7f1edda2
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-26 19:11:29 +0100

    fall back to env vars if local credentials are unavailable

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -3,16 +3,13 @@ import credentials from "../../serviceAccountKey.json";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    // @ts-expect-error The ServiceAccount type wrongfully expects `projectId` instead of `project_id`
     credential: admin.credential.cert({
-      ...(credentials ?? {
-        type: process.env.FIREBASE_TYPE,
-        client_id: process.env.CLIENT_ID,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
-      }),
+      // @ts-expect-error The ServiceAccount type wrongfully expects `projectId` instead of `project_id`
+      client_id: credentials.client_id ?? process.env.CLIENT_ID,
+      client_email: credentials.client_email ?? process.env.FIREBASE_CLIENT_EMAIL,
+      project_id: credentials.project_id ?? process.env.FIREBASE_PROJECT_ID,
+      private_key_id: credentials.private_key_id ?? process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: credentials.private_key ?? process.env.FIREBASE_PRIVATE_KEY,
     }),
     databaseURL: "https://music-quiz-e2298-default-rtdb.europe-west1.firebasedatabase.app",
   });
