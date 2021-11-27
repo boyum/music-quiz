4f996bb27b1c7f31c73f4ce3d4376b2dfdf2b9b5
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-27 13:06:59 +0100

    seperate WinFeedback into its own file

---
+++ web/src/components/win-feedback/WinFeedback.tsx
@@ -0,0 +1,42 @@
+import { CalendarDay } from "../../types/CalendarDay";
+
+export type WinFeedbackProps = {
+  day: CalendarDay;
+};
+
+export const WinFeedback: React.FC<WinFeedbackProps> = ({ day }) => {
+  return (
+    <div className="flex-grow py-20 w-full max-w-sm">
+      <h2 className="text-4xl">Congratulations, you are correct!</h2>
+      <p className="my-10 text-xl">
+        The song was:
+        <div className="mt-2">
+          <span className="underline text-xl">{day.songTitles[0]}</span> by{" "}
+          <span className="underline text-xl">{day.artists[0]}</span>
+        </div>
+      </p>
+      {day.artists.length > 1 ? (
+        <>
+          <p className="my-8 text-xl">These has also made versions of the song:</p>
+          <ul className="list-disc">
+            {day.artists.slice(1).map(artist => (
+              <li key={artist}>{artist}</li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+
+      {day.songTitles.length > 1 ? (
+        <>
+          <p className="my-10 text-xl">Other versions of the song are called:</p>
+          <ul className="list-disc">
+            {day.songTitles.slice(1).map(songTitle => (
+              <li key={songTitle}>{songTitle}</li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+      <p className="text-xl">Get ready for a new task tomorrow 🎁</p>
+    </div>
+  );
+};
--- web/src/pages/day/[dayIndex].tsx
+++ web/src/pages/day/[dayIndex].tsx
@@ -8,6 +8,7 @@ import { useLocalStorage, useWindowSize } from "react-use";
 import { adjectives, animals, colors, uniqueNamesGenerator } from "unique-names-generator";
 import { Hints } from "../../components/hints/Hints";
 import { PauseIcon, PlayIcon } from "../../components/icons/Icons";
+import { WinFeedback } from "../../components/win-feedback/WinFeedback";
 import { CalendarDay } from "../../types/CalendarDay";
 import { FormInputMode } from "../../types/FormInputMode";
 import {
@@ -174,38 +175,7 @@ const DayPage: NextPage<DayPageProps> = ({
           <h1 className="mb-6 text-3xl">{title}</h1>
         </header>
         {showCorrectFeedbackMessage ? (
-          <div className="flex-grow py-20 w-full max-w-sm">
-            <h2 className="text-4xl">Congratulations, you are correct!</h2>
-            <p className="my-10 text-xl">
-              The song was:
-              <div className="mt-2">
-                <span className="underline text-xl">{day.songTitles[0]}</span> by{" "}
-                <span className="underline text-xl">{day.artists[0]}</span>
-              </div>
-            </p>
-            {day.artists.length > 1 ? (
-              <>
-                <p className="my-10 text-xl">These has also made versions of the song:</p>
-                <ul className="list-disc">
-                  {day.artists.slice(1).map(artist => (
-                    <li key={artist}>{artist}</li>
-                  ))}
-                </ul>
-              </>
-            ) : null}
-
-            {day.songTitles.length > 1 ? (
-              <>
-                <p className="my-10 text-xl">Other versions of the song are called:</p>
-                <ul className="list-disc">
-                  {day.songTitles.slice(1).map(songTitle => (
-                    <li key={songTitle}>{songTitle}</li>
-                  ))}
-                </ul>
-              </>
-            ) : null}
-            <p className="text-xl">Get ready for a new task tomorrow 🎁</p>
-          </div>
+          <WinFeedback day={day} />
         ) : (
           <div className="flex-grow w-full max-w-sm">
             <audio
