485b37b2227c101eceebfac97f456990a32cba68
Sindre Bøyum <sindre.boyum@gmail.com>
2021-11-27 15:06:30 +0100

    add Google Analytics

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,16 +1,40 @@
-import "../styles/global.css";
-import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import "tailwindcss/tailwind.css";
 import { SnowCanvas } from "../components/snow-canvas/SnowCanvas";
+import "../styles/global.css";
 
 const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div className="bg-green-900">
-      <SnowCanvas />
-      <div className="z-10 relative">
-        <Component {...pageProps} />
+    <>
+      <Head>
+        {process.env.SANITY_DATASET === "production" ? (
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-CZVWH2Q7LV"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-CZVWH2Q7LV');
+                `}
+            </Script>
+          </>
+        ) : null}
+      </Head>
+
+      <div className="bg-green-900">
+        <SnowCanvas />
+        <div className="relative z-10">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
