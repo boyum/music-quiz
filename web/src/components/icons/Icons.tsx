c9e2b521a476a3921e4776151457117b7c02a8de
Henriette Rudi Moe <henriette.moe@evry.com>
2021-11-29 16:08:14 +0100

    Invert play and pause button color

--- web/src/components/icons/Icons.tsx
+++ web/src/components/icons/Icons.tsx
@@ -11,13 +11,13 @@ export const PlayIcon: React.FC<PlayIconProps> = ({ className }) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="none"
     strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <circle cx="12" cy="12" r="10"></circle>
-    <polygon points="10 8 16 12 10 16 10 8"></polygon>
+    <circle cx="12" cy="12" r="10" fill="currentColor"></circle>
+    <polygon points="10 8 16 12 10 16 10 8" fill="#fff"></polygon>
   </svg>
 );
 
@@ -35,14 +35,14 @@ export const PauseIcon: React.FC<PauseIconProps> = ({ className }) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="none"
     strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <circle cx="12" cy="12" r="10"></circle>
-    <line x1="10" y1="15" x2="10" y2="9"></line>
-    <line x1="14" y1="15" x2="14" y2="9"></line>
+    <circle cx="12" cy="12" r="10" fill="currentColor"></circle>
+    <line x1="10" y1="15" x2="10" y2="9" stroke="#fff"></line>
+    <line x1="14" y1="15" x2="14" y2="9" stroke="#fff"></line>
   </svg>
 );
 
