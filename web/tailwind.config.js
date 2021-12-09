b6b53ec3aefd6d1cf566e2c660395791ebab789e
Sindre Bøyum <sindre.boyum@gmail.com>
2021-12-09 12:00:07 +0100

    purge unused Tailwind classes in production

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,8 +2,7 @@
 
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-  purge: [],
-  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
