131584eab120d0751f7f571deddc61b7e1936c2f
Sindre Bøyum <sindre.boyum@gmail.com>
2021-12-10 08:35:17 +0100

    upgrade TailWind to v3

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,15 +2,11 @@
 
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       serif: ["Lora", "ui-serif", "Georgia", "Cambria", "serif"],
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/aspect-ratio")],
 };
