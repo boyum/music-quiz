8acea258a8c35dde47aafb55c49b89be98cf2242
Sindre Bøyum <sindre.boyum@gmail.com>
2021-12-13 15:53:40 +0100

    add Google Tag Manager

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import "tailwindcss/tailwind.css";
 import { SnowCanvas } from "../components/snow-canvas/SnowCanvas";
 import "../styles/global.css";
-import { IdProvider } from "@radix-ui/react-id"
+import { IdProvider } from "@radix-ui/react-id";
 
 const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -17,17 +17,25 @@ const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
           />
           <Script id="google-analytics" strategy="beforeInteractive">
             {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-GGY21QWYBC');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WLD4SPB');
             `}
           </Script>
         </>
       </Head>
 
       <div className="bg-blue-200">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLD4SPB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <SnowCanvas />
         <div className="relative z-10">
           <Component {...pageProps} />
