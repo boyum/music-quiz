738d81c63a0ce33a8a8584f4cdc72669500a1e4f
Sindre Bøyum <sindre.boyum@gmail.com>
2021-12-14 08:19:51 +0100

    fix tag manager eslint error

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -6,20 +6,15 @@ import "../styles/global.css";
 import { IdProvider } from "@radix-ui/react-id";
 
 const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const tagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WLD4SPB');`;
+
   return (
     <IdProvider>
       <Head>
         <>
-          <script
-            src="https://www.googletagmanager.com/gtag/js?id=G-GGY21QWYBC"
-          ></script>
-          <script>
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WLD4SPB');
-          </script>
+          {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+          <script src="https://www.googletagmanager.com/gtag/js?id=G-GGY21QWYBC"></script>
+          <script dangerouslySetInnerHTML={{ __html: tagManagerScript }}></script>
         </>
       </Head>
 
