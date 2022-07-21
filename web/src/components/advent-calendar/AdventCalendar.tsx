98c7d86aec3f7c90f140f8663e1e630fed348f00
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-07-22 00:04:53 +0200

    remove dialog

--- web/src/components/advent-calendar/AdventCalendar.tsx
+++ web/src/components/advent-calendar/AdventCalendar.tsx
@@ -1,47 +1,19 @@
-import { useCallback, useEffect, useRef, useState } from "react";
 import shuffle from "shuffle-seed";
-import { randomElement } from "../../utils/array.utils";
 import { CalendarDoor } from "../calendar-door/CalendarDoor";
-import { Dialog } from "../dialog/Dialog";
 
 export type AdventCalendarProps = {
-  numberOfUnlockedDays: number;
   finishedDays: Array<number>;
 };
 
-const illegalDoorMessages: Readonly<Array<string>> = [
-  "Are you sure that's the correct date?",
-  "Noooo, be patient - the day will come soon enough.",
-  "Start with today's task instead of focusing on the future.",
-  "⚠️ STOP ⚠️",
-  "You know what they say - good things come to those who wait.",
-  "I'd also like to know what's behind that door. Better wait!",
-];
-
-export const AdventCalendar: React.FC<AdventCalendarProps> = ({
-  numberOfUnlockedDays,
-  finishedDays,
-}) => {
-  const [dialogIsOpen, setDialogIsOpen] = useState(false);
-  const [illegalDoorMessage, setIllegalDoorMessage] = useState("");
-
+export const AdventCalendar: React.FC<AdventCalendarProps> = ({ finishedDays }) => {
   let days = Array(24)
     .fill(null)
     .map((_, index) => ({
       index: index + 1,
-      isUnlocked: index < numberOfUnlockedDays,
       isFinished: finishedDays.includes(index + 1),
     }));
 
   days = shuffle.shuffle(days, "🎄🎄🎄");
-
-  const onClick = useCallback((event: React.MouseEvent, isUnlocked: boolean) => {
-    if (!isUnlocked) {
-      event.preventDefault();
-      setDialogIsOpen(true);
-      setIllegalDoorMessage(randomElement(illegalDoorMessages));
-    }
-  }, []);
 
   return (
     <>
@@ -51,20 +23,11 @@ export const AdventCalendar: React.FC<AdventCalendarProps> = ({
         </h1>
         <p className="mb-8 text-center text-xl text-blue-900">Can you guess them all?</p>
         <div className="mx-auto grid max-w-5xl grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-4 lg:grid-cols-6">
-          {days.map(({ index, isUnlocked, isFinished }) => (
-            <CalendarDoor
-              key={index}
-              index={index}
-              isUnlocked={isUnlocked}
-              isFinished={isFinished}
-              onClick={event => onClick(event, isUnlocked)}
-            />
+          {days.map(({ index, isFinished }) => (
+            <CalendarDoor key={index} index={index} isFinished={isFinished} />
           ))}
         </div>
       </div>
-      <Dialog isOpen={dialogIsOpen} onOpenChange={setDialogIsOpen}>
-        <div className="px-3 py-2 text-xl text-blue-900">{illegalDoorMessage}</div>
-      </Dialog>
     </>
   );
 };
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,13 +3,12 @@ import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import { SnowCanvas } from "../components/snow-canvas/SnowCanvas";
 import "../styles/global.css";
-import { IdProvider } from "@radix-ui/react-id";
 
 const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
   const tagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WLD4SPB');`;
 
   return (
-    <IdProvider>
+    <>
       <Head>
         <>
           {/* eslint-disable-next-line @next/next/no-sync-scripts */}
@@ -32,7 +31,7 @@ const MusicQuiz: React.FC<AppProps> = ({ Component, pageProps }) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </IdProvider>
+    </>
   );
 };
 
