a79a1223bb55f4c580c5a609b7719eed2daeb84a
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-10-14 07:08:21 +0200

    build: remove `eslint-config-standard-react`

--- studio/.eslintrc.js
+++ studio/.eslintrc.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  extends: ['standard', 'standard-react', "prettier"],
-  parser: 'babel-eslint',
+  extends: ["standard", "prettier"],
+  parser: "babel-eslint",
   settings: {
     react: {
-      pragma: 'React',
-      version: '16.14.0'
-    }
-  }
-}
+      pragma: "React",
+      version: "17.0.2",
+    },
+  },
+};
