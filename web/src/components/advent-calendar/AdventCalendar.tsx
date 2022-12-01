56e0301af8a3fcf2631c1c89f3524713ec3baf29
Sindre Bøyum <boyum@users.noreply.github.com>
2022-12-01 10:16:34 +0100

    feat: turn on date lock (#811)

--- web/src/components/advent-calendar/AdventCalendar.tsx
+++ web/src/components/advent-calendar/AdventCalendar.tsx
@@ -1,19 +1,24 @@
-import shuffle from "shuffle-seed";
-import { CalendarDoor } from "../calendar-door/CalendarDoor";
+import shuffle from 'shuffle-seed';
+import { CalendarDoor } from '../calendar-door/CalendarDoor';
 
 export type AdventCalendarProps = {
   finishedDays: Array<number>;
+  date: number;
 };
 
-export const AdventCalendar: React.FC<AdventCalendarProps> = ({ finishedDays }) => {
+export const AdventCalendar: React.FC<AdventCalendarProps> = ({
+  finishedDays,
+  date,
+}) => {
   let days = Array(24)
     .fill(null)
     .map((_, index) => ({
       index: index + 1,
       isFinished: finishedDays.includes(index + 1),
+      isUnlocked: date > index,
     }));
 
-  days = shuffle.shuffle(days, "🎄🎄🎄");
+  days = shuffle.shuffle(days, '🎄🎄🎄');
 
   return (
     <>
@@ -21,10 +26,17 @@ export const AdventCalendar: React.FC<AdventCalendarProps> = ({ finishedDays }) 
         <h1 className="mb-4 mt-4 text-center text-3xl text-blue-900 md:mb-10 md:mt-6 md:text-5xl lg:mb-12 lg:mt-16 xl:text-6xl">
           🎄 Jingle Bell Rock 🎄
         </h1>
-        <p className="mb-8 text-center text-xl text-blue-900">Can you guess them all?</p>
+        <p className="mb-8 text-center text-xl text-blue-900">
+          Can you guess them all?
+        </p>
         <div className="mx-auto grid max-w-5xl grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-4 lg:grid-cols-6">
-          {days.map(({ index, isFinished }) => (
-            <CalendarDoor key={index} index={index} isFinished={isFinished} />
+          {days.map(({ index, isFinished, isUnlocked }) => (
+            <CalendarDoor
+              key={index}
+              index={index}
+              isFinished={isFinished}
+              isUnlocked={isUnlocked}
+            />
           ))}
         </div>
       </div>
