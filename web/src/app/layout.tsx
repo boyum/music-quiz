efa81c4dfc0ee0207fc0faf3385cad385168dfbd
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-05-07 11:09:17 +0200

    feat: add `lang="en"` to html

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,7 +7,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   const tagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WLD4SPB');`;
 
   return (
-    <html>
+    <html lang="en">
       <head>
         <link
           rel="icon"
