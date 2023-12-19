b99c9e2eb7cd35396581d159650a09f7db3eeee8
Sindre Bøyum <boyum@users.noreply.github.com>
2023-12-19 21:43:51 +0100

    chore: commonjs -> esm (#1735)

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,8 @@
 // @ts-check
+import aspectRatioPlugin from "@tailwindcss/aspect-ratio";
 
 /** @type {import("tailwindcss").Config} */
-module.exports = {
+const config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -12,5 +13,7 @@ module.exports = {
       serif: ["Lora", "ui-serif", "Georgia", "Cambria", "serif"],
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [aspectRatioPlugin],
 };
+
+export default config;
