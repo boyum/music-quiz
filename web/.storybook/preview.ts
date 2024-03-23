051788789d6d45d0983b03cbc62d57cd339fd073
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-03-23 17:00:40 +0100

    chore: upgrade to storybook 8

--- web/.storybook/preview.ts
+++ web/.storybook/preview.ts
@@ -1,9 +1,7 @@
 import type { Parameters } from "@storybook/react";
-import "tailwindcss/tailwind.css";
 import "../src/styles/global.css";
 
 export const parameters: Parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
