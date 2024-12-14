14673ada3779c275841e4c25d57ed0b028a2f4f3
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-12-14 09:46:05 +0100

    chore: fix format

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/next";
 import type { FC, PropsWithChildren } from "react";
 import { Footer } from "../components/footer/Footer";
 import { SnowCanvas } from "../components/snow-canvas/SnowCanvas";
 import "../styles/global.css";
-import { Analytics } from "@vercel/analytics/next"
-
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   const tagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WLD4SPB');`;
