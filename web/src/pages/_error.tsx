61f6b570b7bfbbce77f8d36a674fdab2d495edd2
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-12-16 12:55:29 +0100

    feat: add sentry for frontend logging

---
+++ web/src/pages/_error.tsx
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/nextjs";
+import type { NextPageContext } from "next";
+import NextError from "next/error";
+import type { ComponentProps } from "react";
+
+const CustomErrorComponent = (props: ComponentProps<typeof NextError>) => {
+  return <NextError statusCode={props.statusCode} />;
+};
+
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  await Sentry.captureUnderscoreErrorException(contextData);
+
+  // This will contain the status code of the response
+  return NextError.getInitialProps(contextData);
+};
+
+export default CustomErrorComponent;
