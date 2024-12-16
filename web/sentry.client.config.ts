739bd22a4f65fd5680533ea834194313070745ec
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-12-16 12:57:56 +0100

    chore: fix format

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -1,16 +1,13 @@
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://f7b446e5ad6e4580b9043d174957af31@o1021566.ingest.us.sentry.io/5987554",
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
