e58cd7fb5dc94fbe7c4d59903326a83e237ce8e5
Sindre Bøyum <boyum@users.noreply.github.com>
2024-12-16 13:01:37 +0100

    feat: add sentry for frontend logging (#2084)
    * feat: add sentry for frontend logging
    
    * chore: fix format
    
    ---------
    
    Co-authored-by: Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f7b446e5ad6e4580b9043d174957af31@o1021566.ingest.us.sentry.io/5987554",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
